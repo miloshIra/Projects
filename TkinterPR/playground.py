4db477c57f27e89212876b3638671be36fd09a4a
miloshIra <maritonski@gmail.com>
2021-10-05 20:47:54 +0200

    Tkinter miles to km converter for fun :)

---
+++ TkinterPR/playground.py
@@ -0,0 +1,28 @@
+def add(*args):
+        print(sum(args))
+
+
+add(10,20,30,40,3,1,3,4,5,76,23,3,1,1,)\
+
+def calculate(n, **kwargs):
+    print(kwargs)
+    # for key, value in kwargs.items():
+    #     print(key)
+    #     print(value)
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    print(n)
+
+
+calculate(2, add=3, multiply=5)
+
+
+class Car:
+    def __init__(self, **kw):
+        self.make = kw.get("make")
+        self.model = kw.get("model")
+        self.colour = kw.get("colour")
+        self.seats = kw.get("seats")
+
+my_car = Car(make="Nissan",seats="5")
+print(my_car.make, my_car.seats)
